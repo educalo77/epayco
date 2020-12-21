fdb7238373156e0e76f78c3cbf0db24ef444784f
educalo77 <educalo@outlook.com>
2020-12-21 19:16:56 -0300

    faltan funcionalidadesd de recarga y pago

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -14,7 +14,20 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import "./styles.css"
 import { grey } from '@material-ui/core/colors';
-import Sidebar from './Sidebar';
+import clsx from 'clsx';
+import { useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import PaymentIcon from '@material-ui/icons/Payment';
+import MoneyIcon from '@material-ui/icons/Money';
 
 function Copyright() {
   return (
@@ -29,17 +42,73 @@ function Copyright() {
   );
 }
 
+const drawerWidth = 240;
+
 const useStyles = makeStyles((theme) => ({
-root: {
-    flexGrow: 1,
-    backgroundColor: grey
-},
-menuButton: {
+    root: {
+      flexGrow: 1,
+      backgroundColor: grey
+  },
+  menuButton: {
+      marginRight: theme.spacing(2),
+  },
+  title: {
+      flexGrow: 1,
+  },
+
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
     marginRight: theme.spacing(2),
-},
-title: {
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    marginTop: '69px'
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
     flexGrow: 1,
-},
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
 }));
 
 
@@ -48,7 +117,8 @@ const Dashboard = () => {
 
   //  { setAuth }
 
-    const classes = useStyles();
+  const classes = useStyles();
+    const theme = useTheme();
     const [auth, setAuth] = React.useState(true);
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
@@ -106,14 +176,19 @@ const Dashboard = () => {
         window.location = "/"
     }
 
-  const onClick = (e) => {
-    return (
-      <Sidebar />
-    )
-    }
+
+  const [opened, setOpened] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpened(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpened(false);
+  };
   
     return (
-         <div className={classes.root} >
+         <div >
       {/* <FormGroup>
         <FormControlLabel
           control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
@@ -122,14 +197,15 @@ const Dashboard = () => {
       </FormGroup> */}
       <AppBar position="static" >
         <Toolbar className="appbar">
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon onClick={(e) => onClick} />
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleDrawerOpen} >
+            <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            <img height="16px" src="https://epayco.co/img/logo_fondo_epayco_davi.png" alt=""  />
+            <img height="16px" src="https://epayco.co/img/logo_fondo_epayco_davi.png" alt="" />
           </Typography>
           {auth && (
-            <div>
+              <div className="image_profile">
+                <h5 className="text">Bienvenido {name}</h5>
               <IconButton
                 aria-label="account of current user"
                 aria-controls="menu-appbar"
@@ -163,8 +239,33 @@ const Dashboard = () => {
             </div>
           )}
         </Toolbar>
-            </AppBar>
-        <Box pt={4}>
+        </AppBar>
+              <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={opened}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Pagar', 'Recargar'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <PaymentIcon /> : <MoneyIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+        <Box pt={4} className="copy" >
         <Copyright />
         </Box>
     </div>
@@ -184,6 +285,7 @@ const Dashboard = () => {
     // </Fragment>
     );
 };
+
 
 
 export default Dashboard;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,7 +4,69 @@ import { toast } from 'react-toastify';
 import GoogleLogin from 'react-google-login';
 import { useDispatch } from "react-redux";
 import { getUser } from "../components/redux/useraction";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Links from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import "./styles.css"
+import { grey } from '@material-ui/core/colors';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://epayco.co/">
+        e-PayCo
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+      margin: theme.spacing(1),
+      backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[700] : theme.palette.grey[900],
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+      margin: theme.spacing(3, 0, 2),
+      backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[700] : theme.palette.grey[900],
+  },
+}));
 
 const Login = ({ setAuth }) => {
     
@@ -64,10 +126,11 @@ const Login = ({ setAuth }) => {
             toast.error("Login Error");
         }
     };
+      const classes = useStyles();
 
     return ( 
-    <Fragment>
-            <h1 className="text-center my-5">Login</h1>
+    <div className="login" >
+            {/* <h1 className="text-center my-5">Login</h1>
             <form onSubmit={onSubmitForm}>
                 <input
                     type="email"
@@ -99,8 +162,88 @@ const Login = ({ setAuth }) => {
             cookiePolicy={'single_host_origin'}
             className="googlebot"
             />
-            </div>
-    </Fragment>
+            </div> */}
+
+            <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={onSubmitForm}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={e => onChange(e)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={e => onChange(e)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+                            />
+            <GoogleLogin
+            clientId="274236062060-1vk0mne3n6li5bgj5lu4sruoa2agrp2l.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+            className="googlebot"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Links href="#" variant="body2">
+                  Forgot password?
+                </Links>
+               </Grid>
+            
+              <Grid item>
+                <Links href="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Links>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+      </Grid>
+    </div>
     );
 };
 
